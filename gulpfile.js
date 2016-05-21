f8aaf192f2eeff701dfdac128c88e8f19ca21ee8
Octopus in vitro <octopusinvitro@users.noreply.github.com>
2016-05-21 01:34:17 +0100

    Add gulp to concat/minify JS and serve the specs page

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var
+browsersync  = require('browser-sync').create(),
+  concat     = require('gulp-concat'),
+  gulp       = require('gulp'),
+  sourcemaps = require('gulp-sourcemaps'),
+  uglify     = require('gulp-uglify'),
+  paths      = {
+    jsdev:   [
+    ],
+    jsdist:  './javascripts/',
+  };
+
+gulp.task('js', function() {
+  return gulp
+    .src(paths.jsdev)
+    .pipe(sourcemaps.init())
+    .pipe(concat('downloader-bundle.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.jsdist));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.jsdev, ['js']);
+});
+
+gulp.task('server', function() {
+  browsersync.init({
+    server: {
+      baseDir: './',
+      routes: {
+        "/test" : 'javascripts-dev'
+      }
+    },
+    port:   4000,
+    notify: false,
+    open:   false
+  });
+});
+
+gulp.task('default', ['js', 'watch', 'server']);
