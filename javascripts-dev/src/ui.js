7bbf1f8eb3db6db2f9c6d449565bd73c89dc435a
Jodi Winters <github@kodikoscomputing.com>
2016-06-03 19:05:14 +0100

    WIP: building up file list location

--- javascripts-dev/src/ui.js
+++ javascripts-dev/src/ui.js
@@ -1,16 +1,18 @@
 var UserInterface = {
-  selector: "a[href|=#download]"
+  selector: 'a[href|="#download"]'
 };
 
 UserInterface.setup = function(zipper, downloader) {
 
-  var downloadLinks = document.body.querySelector(UserInterface.selector);
-console.log(downloadLinks);
+  var downloadLinks = document.body.querySelectorAll(UserInterface.selector);
+
   var createZip = function() {
     zipper.createZip(downloader);
   };
 
   var registerListener = function(link) {
+    console.log("adding listener to", link);
+  
     link.addEventListener("click", function(event) {
       event.preventDefault();
 console.log('creating zip');
@@ -20,10 +22,48 @@ console.log('creating zip');
 
   var checkIfDownloadLinkExist = function() {
     if (downloadLinks) {
-      for (each downloadLinks as linkIdx)
-      registerListener(downloadLinks[linkIdx]);
+      foreachSelector(downloadLinks, function() {
+        registerListener(this);
+
+        hideAccessibleFileLinks(this);
+      });
     }
   };
+
+  var locateAnchorForDownloadLink = function(link) {
+    var anchorName = link.href.replace(/^[^#]*#/, '');
+console.log('looking for anchor', anchorName);
+    return document.body.querySelector('a[name="' + anchorName + '"')
+  }
+
+  var hideAccessibleFileLinks = function(link) {
+    var anchor = locateAnchorForDownloadLink(link);
+console.log('found anchor', anchor);
+    var next = getNextSiblingInDom(anchor);
+    console.log('found sibling', next);
+  };
+
+  function foreachSelector(nodelist, callback) {
+    for (var idx = 0; idx < nodelist.length; idx++) {
+      callback.apply(nodelist[idx]);
+    }
+  }
+
+  function getNextSiblingInDom(element) {
+    var allChildrenOfParent = element.parentNode.children;
+    var nextSibling = false;
+
+    foreachSelector(allChildrenOfParent, function() {
+      if (this === element) {
+        nextSibling = true;
+      } else {
+        if (nextSibling === true) {
+          nextSibling = this;
+        }
+      }
+    });
+    return nextSibling;
+  }
 
   checkIfDownloadLinkExist();
 }
